cc6c0350aeb5b05ce221897f15e6268bbecce23f
joel.r.colina.vargas <joel.colina.25@gmail.com>
2024-07-23 21:09:41 -0400

    Paso1 23-07-2024

--- src/main/java/com/mindhub/homebanking/service/implentacion/AddressesServiceImpl.java
+++ src/main/java/com/mindhub/homebanking/service/implentacion/AddressesServiceImpl.java
@@ -1,25 +1,20 @@
 package com.mindhub.homebanking.service.implentacion;
 
 import com.mindhub.homebanking.dtos.AddressesDTO;
-import com.mindhub.homebanking.repositories.AddressesRepository;
 import com.mindhub.homebanking.service.AddressesService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AddressesServiceImpl implements AddressesService {
 
-    @Autowired
-    private AddressesRepository addressesRepository;
-
     @Override
     public Set<AddressesDTO> finAll() {
-        return this.addressesRepository.findAll().stream().map(AddressesDTO::new).collect(Collectors.toSet());
+        return Set.of();
     }
 
     @Override
     public AddressesDTO finById(Long id) {
-        return this.addressesRepository.findById(id).map(AddressesDTO::new).orElse(null);
+        return null;
     }
 }
